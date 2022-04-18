5a9c80614721eb9d68dafb0f0fa71e69291f832b
Jotham <jothamksamuel@gmail.com>
2022-04-18 13:52:58 -0400

    added files

---
+++ login.js
@@ -0,0 +1,21 @@
+
+
+
+
+    function verify(){
+        var mail = document.getElementById('email').value;
+        var uname = document.getElementById('username').value;
+        var pass = document.getElementById('password').value;
+        var first = document.getElementById('fname').value;
+        var last = document.getElementById('lname').value;
+        var man = document.getElementById('male').value;
+        var woman = document.getElementById('female').value;
+        var one = document.getElementById('single').value;
+        var two = document.getElementById('married').value;
+        var accept = document.getElementById('tos').value;
+
+        console.log(mail,uname,pass,first,last,man,woman,one,two,accept);
+    
+
+
+    }
